142a5ddb83717a5738baec49b4e51c02f676ff3d
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2023-04-08 19:07:57 +0300

    completeSignUp

--- Manav/Controllers/SignUpViewController.swift
+++ Manav/Controllers/SignUpViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Firebase
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController,UINavigationControllerDelegate {
     
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var surnameTextField: UITextField!
@@ -21,11 +21,32 @@ class SignUpViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+    var alertController = UIAlertController()
     @IBAction func signUpButton(_ sender: Any) {
-        Auth.auth().createUser(withEmail: emailTextField.text!, password: confirmPasswordTextField.text!)
+        if emailTextField.text != "" && passwordTextField.text != "" && passwordTextField.text == confirmPasswordTextField.text{
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { result, error in
+                if error == nil {
+                    self.dismiss(animated: true, completion: nil) //back to page
+                }
+                else{
+                    self.errorAlert(error: error?.localizedDescription ?? "")
+                }
+            }
+        }
+        else{
+            self.errorAlert(error: "Kullanıcı Adı ve ya Sifre Yok/Eşleşmiyor")
+        }
+      
     }
-    
    
+    func errorAlert(error : String){
+           alertController = UIAlertController(title: "Hata", message: error, preferredStyle: .alert)
+           let okButton = UIAlertAction(title: "OK", style: .cancel)
+           alertController.addAction(okButton)
+           present(alertController, animated: true)
+       }
 
 }
+
+   
+
